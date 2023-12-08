d37f639a774beb4f4aa95507c42f265783d5da77
Reiterovich <reiterovich.andriy@gmail.com>
2023-12-08 16:26:49 +0200

    1.1
    fix bag

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,24 +1,24 @@
 import { useEffect } from 'react';
 
 export const Modal = ({ closeModal, tags, modalImg }) => {
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  });
-
-  const handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      closeModal();
-    }
-  };
-
   const handleBackdropClick = event => {
     if (event.currentTarget === event.target) {
       closeModal();
     }
   };
+
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [closeModal]);
 
   return (
     <div onClick={handleBackdropClick} className="Overlay">
